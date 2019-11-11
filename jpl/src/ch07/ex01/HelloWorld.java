710d4035ed38ece925ff18559fb480bd493b5c07
satokodai <kodaisato551@gmail.com>
2019-11-11 21:42:33 +0900

    ch07.ex01

--- jpl/src/ch07/ex01/HelloWorld.java
+++ jpl/src/ch07/ex01/HelloWorld.java
@@ -1,3 +1,12 @@
+package ch07.ex01;
+
 /**
  * TODO M:impl
  */
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println(
+				"\u005C\u0022\uFF28\uFF45\uFF4C\uFF4C\uFF4F\uFF0C\u0020\uFF37\uFF4F\uFF52\uFF4C\uFF44\u005C\u0022");
+	}
+}
