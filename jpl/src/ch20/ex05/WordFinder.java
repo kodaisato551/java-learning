cbef4781d332d301deb1bfbf38c5d803ab8fbd57
satokodai <kodaisato551@gmail.com>
2020-04-01 09:38:23 +0900

    ch20.ex05

---
+++ jpl/src/ch20/ex05/WordFinder.java
@@ -0,0 +1,31 @@
+package ch20.ex05;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+
+public class WordFinder {
+    private static final String FILE_LOC = "C:\\study\\java\\java-learning\\jpl\\src\\ch20\\ex05\\read.txt";
+    private static final String KEYWORD = "one day";
+
+    private static void fileReader() throws IOException {
+        FileReader fileIn = new FileReader(FILE_LOC);
+        LineNumberReader in = new LineNumberReader(fileIn);
+        String str;
+
+        while ((str = in.readLine()) != null) {
+            if (str.contains(KEYWORD)) {
+                str = in.getLineNumber() + " " + str;
+            }
+            System.out.println(str);
+        }
+        in.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        fileReader();
+    }
+}
+
+
+
