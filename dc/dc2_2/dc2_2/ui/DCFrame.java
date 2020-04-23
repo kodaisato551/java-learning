5dd159a75940e20d6065222321ecef88d41aca25
滉大 佐藤 <kodai.sato@jp.ricoh.com>
2020-04-23 20:36:17 +0900

    add any way

---
+++ dc/dc2_2/dc2_2/ui/DCFrame.java
@@ -0,0 +1,54 @@
+package dc2_2;
+
+import dc2_2.setting.DefaultSetting;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class DCFrame extends JFrame {
+
+    private MenuBar menuBar;
+    private Menu menu;
+
+    private ActionListener GO_PROPERTY_SETTING = e -> {
+
+    };
+
+    private DCFrame() {
+        super("Digital Clock");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(DefaultSetting.DEFAULT_WINDOW_WIDTH, DefaultSetting.DEFAULT_WINDOW_HEIGHT);
+        DCPanel panel = new DCPanel();
+        setVisible(true);
+        getContentPane().add(panel);
+        setLayouts();
+    }
+
+    private void setLayouts() {
+        menuBar = new MenuBar();
+        menu = new Menu("Property Setting");
+        menuBar.add(menu);
+        setMenuBar(menuBar);
+    }
+
+    private void setListeners() {
+        menu.addActionListener(GO_PROPERTY_SETTING);
+    }
+
+
+    /**
+     * アプリケーションを実行
+     */
+    private void exec() {
+        Timer timer = new Timer(1000, e -> {
+            repaint();
+        });
+        timer.start();
+    }
+
+    public static void main(String[] args) {
+        new DCFrame().exec();
+    }
+
+}
---
+++ dc/dc2_2/dc2_2/util/Utils.java
@@ -0,0 +1,22 @@
+package dc2_2;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class Utils {
+
+    private static final SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+
+    /**
+     * 現在の時刻をformatterのフォーマットに従って成型しStringを返す
+     *
+     * @return String
+     */
+    static String getCurrentTime() {
+        return formatter.format(Calendar.getInstance().getTime());
+    }
+
+    private Utils() {
+    }
+
+}
