1f1bc81f59e41ffc8d7677be17ffb5f44b3226a8
satokodai <kodaisato551@gmail.com>
2020-07-08 00:16:53 +0900

    SettingItemPanel create

--- dc/dc2_4/dc2_4/ui/dialog/SettingItemPanel.java
+++ dc/dc2_4/dc2_4/ui/dialog/SettingItemPanel.java
@@ -1,6 +1,7 @@
 package dc2_4.ui.dialog;
 
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
 import java.awt.*;
 import java.util.List;
 
@@ -22,9 +23,35 @@ public class SettingItemPanel<T> extends JPanel {
         setLayout(new FlowLayout());
         JLabel settingNameJLabel = new JLabel(settingName);
         add(settingNameJLabel);
-
-
+        defaultListModel = new DefaultListModel<>();
+        for (T elem : supportedSettingList) {
+            defaultListModel.addElement(elem);
+        }
+        supportedSettingJList = new JList<>();
+        supportedSettingJList.setModel(defaultListModel);
         add(supportedSettingJList);
+    }
+
+
+    public static class ColorPanel extends JPanel {
+        private Color color;
+        private String colorName;
+
+        //ui
+        private JPanel colorTip;
+
+        ColorPanel(Color color, String colorName) {
+            this.color = color;
+            this.colorName = colorName;
+            setLayout(new FlowLayout());
+            colorTip.setBorder(new BevelBorder(BevelBorder.LOWERED));
+            colorTip.setBackground(color);
+            add(colorTip);
+            add(new JLabel(colorName));
+        }
+    }
+
+    public static void main(String[] args) {
 
     }
 
