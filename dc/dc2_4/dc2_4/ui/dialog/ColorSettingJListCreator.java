2f15fdd7e2ab491a6f62a59ca1c16e9d33644267
kodai sato <kodai.sato@jp.ricoh.com>
2020-07-09 17:27:49 +0900

    colorSettingList sample class

---
+++ dc/dc2_4/dc2_4/ui/dialog/ColorSettingJListCreator.java
@@ -0,0 +1,21 @@
+package dc2_4.ui.dialog;
+
+import dc2_4.setting.ColorListItem;
+import dc2_4.setting.SupportedSettings;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Map;
+
+public class ColorSettingList {
+
+    public static JList<ColorListItem> createColorJList() {
+        DefaultListModel<ColorListItem> model = new DefaultListModel<>();
+        for (Map.Entry<String, Color> entry : SupportedSettings.SUPPORTED_COLOR.entrySet()) {
+            model.addElement(new ColorListItem(entry.getValue(), entry.getKey()));
+        }
+        JList<ColorListItem> list = new JList<>(model);
+        list.setCellRenderer(new ColorSettingItemPanelRenderer());
+        return list;
+    }
+}
---
+++ dc/dc2_4/dc2_4/ui/dialog/ColorSettingListSampleFrame.java
@@ -0,0 +1,10 @@
+package dc2_4.ui.dialog;
+
+import javax.swing.*;
+
+public class SampleFrame extends JFrame {
+
+    SampleFrame() {
+        setBounds(0, 0, 300, 300);
+    }
+}
