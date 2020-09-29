cca2e194a4c2315b4f55a86595bc1a1bccab41c4
satokodai <kodaisato551@gmail.com>
2020-09-29 19:17:54 +0900

    ch01.ex04

---
+++ js8ri/src/ch01/exx04/FileSorter.java
@@ -0,0 +1,36 @@
+package ch01.exx04;
+
+import java.io.File;
+import java.util.Arrays;
+
+/**
+ * ディレクトリの同一階層に存在するディレクトリ及びファイルのグループそれぞれでソートする
+ */
+public class FileSorter {
+
+    public static void sort(File[] files) {
+        Arrays.sort(files, (f1, f2) -> {
+            if (f1.isDirectory() && !f2.isDirectory()) {
+                return -1;
+            } else if (!f1.isDirectory() && f2.isDirectory()) {
+                return 1;
+            } else {
+                return f1.getPath().compareTo(f2.getPath());
+            }
+        });
+    }
+
+    private static void printFiles(File[] files) {
+        Arrays.stream(files).forEach(System.out::println);
+    }
+
+
+    public static void main(String[] args) {
+        File file = new File("C:\\study\\java\\java-learning\\js8ri");
+        File[] files = file.listFiles();
+        FileSorter.printFiles(files);
+        FileSorter.sort(files);
+        System.out.println();
+        FileSorter.printFiles(files);
+    }
+}
