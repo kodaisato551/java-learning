772877affe97c51bb3931797d7c9284e1dbc6e58
satokodai <kodaisato551@gmail.com>
2020-10-07 08:00:55 +0900

    add

---
+++ js8ri/src/ch02/ex07/Util.java
@@ -0,0 +1,19 @@
+package ch02.ex07;
+
+import java.util.stream.Stream;
+
+/**
+ * TODO TBD
+ */
+public class Util {
+    /**
+     * 有限ストリームかどうか
+     *
+     * @param stream
+     * @param <T>
+     * @return
+     */
+    public static <T> boolean isFinate(Stream<T> stream) {
+        return true;
+    }
+}
---
+++ js8ri/src/ch02/ex08/Main.java
@@ -0,0 +1,19 @@
+package ch02.ex08;
+
+import java.util.stream.Stream;
+
+public class Main {
+    /**
+     * 最初のストリームと次のストリームから交互に要素を取り出し、
+     * どちらかのストリームから要素がなくなったら停止する
+     * TODO tbd
+     *
+     * @param first
+     * @param second
+     * @param <T>
+     * @return
+     */
+    public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
+        return null
+    }
+}
