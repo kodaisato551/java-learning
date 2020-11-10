a45a65954044980e1c20f7e42210d5956487eaa2
satokodai <kodaisato551@gmail.com>
2020-11-10 21:24:44 +0900

    add

--- js8ri/src/ch04/ex01/HelloWorld.java
+++ js8ri/src/ch04/ex01/HelloWorld.java
@@ -3,6 +3,8 @@ package ch04.ex01;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -10,9 +12,14 @@ public class HelloWorld extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Label message = new Label(" Hello, JavaFX!");
+        Label message = new Label("Hello, FX!");
         message.setFont(new Font(100));
-        stage.setScene(new Scene(message));
+        TextField textField = new TextField("Hello, FX!");
+        message.textProperty().bind(textField.textProperty());
+        BorderPane pane = new BorderPane();
+        pane.setBottom(textField);
+        pane.setCenter(message);
+        stage.setScene(new Scene(pane));
         stage.setTitle("Hello");
         stage.show();
     }
