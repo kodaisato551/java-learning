50daeb28e6ff8b1eb80481acd94e8f51d0319058
滉大 佐藤 <kodai.sato@jp.ricoh.com>
2020-11-26 18:09:39 +0900

    ch05.ex04

---
+++ js8ri/src/ch05/ex04/CalenderViewer.java
@@ -0,0 +1,65 @@
+package ch05.ex04;
+
+import java.time.LocalDate;
+import java.util.function.Consumer;
+
+/**
+ * ある 月 の カレンダー を 表示 する Unix の cal プログラム と 同じ プログラム を 書き なさい。
+ * たとえば、 java Cal 3 2013 を 実行 する と、 次 の よう に 表示 し ます。
+ * 3 月 1 日 が 金曜日 で ある こと を 示し て い ます（ 土曜日 と 日曜 が 右端 に 表示 さ れる よう に し なさい）。
+ */
+public class CalenderViewer {
+    private int month;
+    private int year;
+
+    public CalenderViewer(int month, int year) {
+        this.month = month;
+        this.year = year;
+    }
+
+    public void show() {
+        LocalDate date = LocalDate.of(year, month, 1);
+        LocalDate endOfMonth = date.plusMonths(1).minusDays(1);//月末
+        date.datesUntil(endOfMonth).forEach(cal());
+    }
+
+    /**
+     * 右端を土日にそろえるConsumerの生成
+     * TODO 思ったのと違う..笑
+     * @return
+     */
+    private static Consumer<LocalDate> cal() {
+        return w -> {
+//            StringBuilder sb = new StringBuilder();
+//            for (int i = 1; i < w.getDayOfWeek().getValue(); i++) {
+//                sb.append(" ");
+//            }
+//            sb.append(w.getDayOfMonth());
+//            if (w.getDayOfWeek().getValue() == 7) {
+//                sb.append("\n");
+//            }
+//            System.out.print(sb);
+
+            if (w.getDayOfMonth() == 1) {
+//                for (int i = 0; i < w.getDayOfWeek().getValue(); i++) {
+//                    System.out.print("  ");
+//                }
+                String formatt = "%" + w.getDayOfWeek().getValue() * 3 + "d";
+                System.out.printf(formatt, w.getDayOfMonth());
+            } else {
+                System.out.printf("%3d", w.getDayOfMonth());
+
+            }
+            if (w.getDayOfWeek().getValue() == 7) {
+                System.out.print("\n");
+            }
+        };
+    }
+
+
+    public static void main(String[] args) {
+        CalenderViewer viewer = new CalenderViewer(3, 2013);
+        viewer.show();
+    }
+
+}
