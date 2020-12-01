ffefc8c74cb5f73e39d1ea171cabd51df294f0eb
滉大 佐藤 <kodai.sato@jp.ricoh.com>
2020-12-01 20:39:12 +0900

    add

--- js8ri/src/ch05/ex06/Jason.java
+++ js8ri/src/ch05/ex06/Jason.java
@@ -1,5 +1,6 @@
 package ch05.ex06;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 /**
@@ -9,9 +10,20 @@ public class Jason {
     /**
      * 13日の金曜を表示する
      */
-    void showBadFriDay(int century) {
-        int year = century * 100 + 1;
+    static void showBadFriDay(int century) {
+        int year = (century - 1) * 100 + 1;
+        int endYear = century * 100;
         LocalDate date = LocalDate.of(year, 1, 13);
+        do {
+            if (date.getDayOfWeek().equals(DayOfWeek.FRIDAY)) {
+                System.out.println(date);
+            }
+            date = date.plusMonths(1);
+        } while (date.getYear() <= endYear);
+    }
 
+
+    public static void main(String[] args) {
+        showBadFriDay(20);
     }
 }
