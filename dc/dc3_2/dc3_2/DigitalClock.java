aaacd92308c03433de0b113b606642b20f82a4bf
kodai sato <kodai.sato@jp.ricoh.com>
2021-01-04 11:14:07 +0900

    add

---
+++ dc/dc3_2/dc3_2/DigitalClock.java
@@ -0,0 +1,54 @@
+package dc3_2;
+
+import dc3_2.setting.DefaultProperties;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ */
+public class DigitalClock extends Application {
+
+    private final Label mLabel = new Label();
+
+    private final EventHandler<ActionEvent> mEventHandler = e -> {
+        String currentTime = getCurrentTime();
+        System.out.println("Current Time :: " + currentTime);
+        mLabel.setText(currentTime);
+        mLabel.setFont(DefaultProperties.FONT);
+    };
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Timeline timer = new Timeline(
+                new KeyFrame(Duration.millis(1000), mEventHandler)
+        );
+        timer.setCycleCount(Timeline.INDEFINITE);
+        timer.play();
+        stage.setScene(new Scene(mLabel, 300, 100));
+        stage.show();
+    }
+
+
+    private static String getCurrentTime() {
+        Date dateObj = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        return format.format(dateObj);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ dc/dc3_2/dc3_2/setting/DefaultProperties.java
@@ -0,0 +1,7 @@
+package dc3_2.setting;
+
+import javafx.scene.text.Font;
+
+public class DefaultProperties {
+    public static Font FONT = new Font("Arial", 30);
+}
